ada4df7383867f0c88631dc45eb68a01be357d28
Taylor Nyquist <tnyquist@gmail.com>
2020-10-07 02:33:05 -0500

    working reactions, need bonus

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -44,7 +44,7 @@ router
 //  /api/thoughts/:thoughtId/reactions/:reactionId
 
 router
-    .route(':thoughtId/reactions/:reactionId')
+    .route('/:thoughtId/reactions/:reactionId')
     .delete(deleteReaction)
 
 module.exports = router;
